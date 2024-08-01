19a1f37dcb458d848ea1780183cc8935ba6d4bf7
Slatinat Dean <deanslatinat@yahoo.com>
2024-08-01 16:06:17 +0300

    added some refinements to Poarta1

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from Poarta1 import *
 
-Dean=Poarta1(1,'in')
-Mihai=Poarta1(2,'in')
-Dean2=Poarta1(1,'out')
-Mihai2=Poarta1(2,'out')
+# Creating instances
+Dean = Poarta1(1, 'in')
+Mihai = Poarta1(2, 'in')
+Dean2 = Poarta1(1, 'out')
+Mihai2 = Poarta1(2, 'out')
+
+# Collecting entries
 Dean.salvareDate()
 Mihai.salvareDate()
 Dean2.salvareDate()
 Mihai2.salvareDate()
+
+# Save all entries to files
+Poarta1.save_all_entries()
